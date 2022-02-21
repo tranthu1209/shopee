f1b40687cf5f17185419538fd4a5677b3eeec564
tranthu1209 <77615114+tranthu1209@users.noreply.github.com>
2022-02-21 20:18:10 +0700

    create slider js

---
+++ assets/js/home.js
@@ -0,0 +1,56 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(k){
+    showDivs(slideIndex += k);
+}
+function currentDiv(k){
+    showDivs(slideIndex = k);
+}
+function showDivs(k) {
+    var i;
+    var x = document.getElementsByClassName("slider-img");
+    var dots = document.getElementsByClassName("slider-btn-index");
+    if (k>x.length){
+        slideIndex = 1;
+    }
+    if (k<1){
+        slideIndex = x.length;
+    }
+    for (i = 0; i <x.length; i++){
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" slider-btn-index-active", "")
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " slider-btn-index-active";
+}
+
+showSlide();
+
+function showSlide() {
+  var i;
+  var slides = document.getElementsByClassName("slider-img");
+  var dots = document.getElementsByClassName("slider-btn-index");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slider-btn-index-active", "")
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " slider-btn-index-active";
+  setTimeout(showSlide, 3000); // Change image every 2 seconds
+}
+
+// 
+
+function moveRight() {
+    var list = document.getElementById("list-sale-product");
+    left = list.style.left;
+    list.style.transform = 'translate(-1000px, 0)';
+    list.style.animation = 'moveRight 2s'
+    
+}
