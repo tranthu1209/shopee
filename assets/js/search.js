cc3c0ace538767b37de84f24d4a7233f13624b99
tranthu1209 <77615114+tranthu1209@users.noreply.github.com>
2022-03-10 15:52:26 +0700

    responsive hone page & search page

---
+++ assets/js/search.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("filter-wrapper").style.width = "90%";
+    document.getElementById("modal").style.display = "block";
+  }
+  
+function closeNav() {
+    document.getElementById("filter-wrapper").style.width = "";
+    document.getElementById("modal").style.display = "none";
+    
+    
+}
